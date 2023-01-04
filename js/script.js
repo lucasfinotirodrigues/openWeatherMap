32e95f340ef810a55e8a6b40ac80c571468607b5
lucas_finoti <lucasfinotirodrigues@hotmail.com>
2023-01-04 00:30:40 -0300

    "Update deixando o projeto 100% funcional"

--- js/script.js
+++ js/script.js
@@ -1,1 +1,58 @@
 //00e6ed7502c83f94639ee465f92a0204
+
+
+
+//Variáveis e seleções do elemento
+
+const apiKey = "00e6ed7502c83f94639ee465f92a0204";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city")
+const TempElement = document.querySelector("#temperature span")
+const descElement = document.querySelector("#description")
+const humidityElement = document.querySelector("#humidity span")
+const windElement = document.querySelector("#wind span")
+
+const weatherContainer = document.querySelector("#weather-data");
+
+
+//Funções
+
+const getWeatherData = async(city) =>{
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
+        const res = await fetch(apiWeatherURL)
+        const data = await res.json()
+
+      return data;
+}
+
+const showWeatherData = async (city) =>{
+    const data = await getWeatherData(city);
+
+    cityElement.innerText = data.name;
+    TempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
+
+    weatherContainer.classList.remove("hide"); 
+}
+
+//Eventos
+searchBtn.addEventListener("click", (e) => {
+
+    e.preventDefault();
+
+    const city = cityInput.value;
+    showWeatherData(city)
+})
+
+cityInput.addEventListener("keyup", (e) => {
+    if(e.code === "Enter"){
+        const city = e.target.value
+
+        showWeatherData(city);
+    }
+})
